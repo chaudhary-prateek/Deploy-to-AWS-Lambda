ea8c9db3d8eae7cce622a8eb5e30c51483978c29
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-29 10:27:42 +0530

    feat:tag.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hi from Lambda in a Docker container! deploy v1.0.1" }),
+    body: JSON.stringify({ message: "Hi from Lambda in a Docker container! deploy tag.2" }),
   };
 };
