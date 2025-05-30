12ffcd5d8400862e28dbe7b901f69b119e01912c
Prateek Chaudhary <prateekchaudhary7805@gmail.com>
2025-05-30 17:04:32 +0530

    feat: update the index file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hi from Lambda in a Docker container! deploy Prateek.DEV_Branch " }),
+    body: JSON.stringify({ message: "Hi from Lambda in a Docker container! deploy Prateek.DEV_Branch TAG2 " }),
   };
 };
